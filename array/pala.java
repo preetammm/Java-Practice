363e8f2a6bb77fee34c477c295047eed975fb35a
PREETAM PRAJAPATI <preetamp765@gmail.com>
2026-02-03 21:46:44 +0530

    palan

---
+++ array/pala.java
@@ -0,0 +1,24 @@
+package array;
+public class pala {
+    
+
+
+
+
+    public static void main(String[]args){
+    StringBuilder a=new StringBuilder("lol");
+    StringBuilder b=new StringBuilder(a);
+    System.out.println(b);
+    b.reverse();
+    System.out.println(b);
+    if (a==b)
+    {
+        System.out.println("palandrom");
+    }
+    else{
+        System.out.println("no palandrom");
+    }
+
+    
+}
+}
