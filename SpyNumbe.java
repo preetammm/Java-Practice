c3cf995a222dc27532a111f74972c47583374503
PREETAM PRAJAPATI <preetamp765@gmail.com>
2026-02-03 21:53:04 +0530

    spynumber

---
+++ SpyNumbe.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class SpyNumbe {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int sum = 0;
+        int product = 1;
+        int temp = num;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += digit;
+            product *= digit;
+            temp /= 10;
+        }
+
+        if (sum == product) {
+            System.out.println(num + " is a Spy Number");
+        } else {
+            System.out.println(num + " is NOT a Spy Number");
+        }
+
+        sc.close();
+    }
+}
